461e89801c0308cd43a6a5165344b2cc304f6a22
John Rogerson <john.rogerson@ge.com>
2015-06-03 15:49:24 -0700

    Configured sassdoc

--- tasks/options/sassdoc.js
+++ tasks/options/sassdoc.js
@@ -1,46 +1,8 @@
 module.exports = {
-
-    // Bare minimum.
     bare: {
-        src: '*.scss'
-    }//,
-
-    // Glob patterns Array.
-//    srcs: {
-//        src: ['*.scss']
-//    },
-
-    // With config passed in as an external file.
-//    config: {
-//        src: ['test/fixture/**/*.scss'],
-//        options: {
-//            config: 'test/config.json'
-//        }
-//    },
-
-    // With config passed in as object.
-//    opts: {
-//        src: '*.scss',
-//        options: {
-//            // cli opts.
-//            verbose: true,
-//            theme: 'default',
-//            // SassDoc opts.
-//            //package: pkg,
-//            autofill: ['requires', 'throws'],
-//            groups: {
-//                'undefined': 'Ungrouped',
-//                'foo': 'Foo group',
-//                'bar': 'Bar group'
-//            },
-//            // theme opts.
-//            display: {
-//                access: ['public', 'private'],
-//                alias: true,
-//                watermark: true
-//            },
-//            basePath: 'https://github.com/SassDoc/grunt-sassdoc/tree/master/test/fixture'
-//        }
-//    }
-
+      src: '*.scss'
+    },
+    options: {
+      dest: 'dist/sassdoc'
+    }
 };
